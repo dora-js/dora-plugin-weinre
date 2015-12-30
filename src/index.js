ccbcc7da17aca6712c4b7ac26ae8d235b06fd2d3
玄寂 <xuanji.jw@taobao.com>
2015-12-30 23:51:41 +0800

    0.1.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,60 @@
+import 'babel-polyfill';
+import { existsSync, readFileSync } from 'fs';
+import { parse } from 'url';
+import { join } from 'path';
+import assign from 'object-assign';
+
+/**
+ * weinre code
+ * https://git-wip-us.apache.org/repos/asf?p=cordova-weinre.git;a=blob;f=weinre.server/lib/cli.js;h=3c4b50784a538af7fdbb471d56f35e11802ef0f4;hb=cc97191c79ca260b5bbf83b19d5cfc3e02468cac
+ */
+require('coffee-script');
+import { run } from 'weinre';
+
+let defaultOpts = {
+  httpPort: 8990,
+  boundHost: 'localhost',
+  verbose: false,
+  debug: false,
+  readTimeout: 5,
+  deathTimeout: 15,
+  help: false,
+};
 
 export default {
+  'middleware.before'() {
+    const { log, localIP } = this;
+    defaultOpts.boundHost = localIP;
+    defaultOpts = assign(defaultOpts, this.query);
 
+    run(defaultOpts);
+    log.info(`weinre is started, servering at http://${defaultOpts.boundHost}:${defaultOpts.httpPort}`);
+  },
+
+  'middleware'() {
+    const { cwd } = this;
+
+    return function* (next) {
+      const fileName = parse(this.url).pathname;
+      const filePath = join(cwd, fileName);
+      const isHTML = /\.html?$/.test(this.url.split('?')[0]);
+      if (isHTML && existsSync(filePath)) {
+        const injectScript = `<script src='http://${defaultOpts.boundHost}:${defaultOpts.httpPort}/target/target-script-min.js#anonymous'></script>`;
+        let content = readFileSync(filePath, 'utf-8');
+        const docTypeReg = new RegExp('^\s*\<\!DOCTYPE\s*.+\>.*$', 'im');
+        const docType = content.match(docTypeReg);
+        if (docType) {
+          content = content.replace(docTypeReg, docType[0] + injectScript);
+          this.body = content;
+
+          return;
+        }
+        content = injectScript + content;
+        this.body = content;
+
+        return;
+      }
+      yield next;
+    };
+  },
 };
--- test/index-test.js
+++ test/index-test.js
@@ -2,23 +2,74 @@ import dora from 'dora';
 import { join } from 'path';
 import request from 'supertest';
 
+const localIP = require('internal-ip')();
 const port = '12345';
 
-describe('index', () => {
-
-  before(done => {
-    dora({
-      port,
-      plugins: [`../../../src/index`],
-      cwd: join(__dirname, './fixtures/normal'),
+describe('index', function() {
+  this.timeout(50000);
+  describe('livereload.js', () => {
+    
+    const cwd = process.cwd();
+    before(done => {
+      process.chdir(join(__dirname, './fixtures/normal'));   
+      dora({
+        port,
+        plugins: ['../../../src/index?{httpPort:8888}'],
+        cwd: join(__dirname, './fixtures/normal'),
+      });
+      setTimeout(done, 1000);
     });
-    setTimeout(done, 1000);
-  });
 
-  it('GET /package.json', done => {
-    request(`http://localhost:${port}`)
-      .get('/package.json')
-      .expect(/\.\/index\.js/, done);
+    after(() => {
+      process.chdir(cwd);
+    });
+
+    it('GET weinre /target/target-script-min.js#anonymous', done => {
+
+      request(`http://${localIP}:8888`)
+        .get('/target/target-script-min.js#anonymous')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          if (res.text.indexOf(`modjewel.require('weinre/target/Target').main()`) < 0) throw new Error("/target/target-script-min.js#anonymous is not correct"); 
+          done();
+        });
+    });
+
+    it('GET /index.html is injected the script /target/target-script-min.js#anonymous', done => {
+      request(`http://localhost:${port}`)
+        .get('/index.html')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          if (res.text.indexOf(`<script src='http://${localIP}:8888/target/target-script-min.js#anonymous'></script>`) < 0) throw new Error("/target/target-script-min.js#anonymous is not injected"); 
+          done();
+        });
+    });
+
+    it('GET /lackdoctype.html is injected the script livereload.js', done => {
+      request(`http://localhost:${port}`)
+        .get('/lackdoctype.html')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          if (res.text.indexOf(`<script src='http://${localIP}:8888/target/target-script-min.js#anonymous'></script>`) < 0) throw new Error("/target/target-script-min.js#anonymous is not injected"); 
+          done();
+        });
+    });
+
+    it('GET /index.js should not be handled', done => {
+      request(`http://localhost:${port}`)
+        .get('/index.js')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          if (res.text.indexOf('console.log(1);') < 0) throw new Error("other types of files should not be handled"); 
+          done();
+        });
+    });
   });
 });
+
+
 
